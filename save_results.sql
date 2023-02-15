9823fbb3515399cc243f092454a50785887a3888
Camila Lopes <camilalopes9742@gmail.com>
2023-02-15 10:25:45 -0300

    creates queries to storage execution results

---
+++ save_results.sql
@@ -0,0 +1,20 @@
+COPY (
+    SELECT
+        st.total_num_clients,
+        st.total_num_examples,
+        st.accuracy,
+        st.loss,
+        st.training_time,
+        st.val_accuracy,
+        st.val_loss, 
+        se.total_num_clients,
+        se.total_num_examples,
+        se.accuracy,
+        se.loss,
+        se.evaluation_time
+    FROM
+        oservertrainingaggregation as st
+    JOIN 
+        oserverevaluationaggregation as se
+        ON st.server_round = se.server_round )
+INTO 'results.csv' ON CLIENT USING DELIMITERS ',', '\n', '"';
---
+++ save_training_config.sql
@@ -0,0 +1,13 @@
+COPY (
+	SELECT
+		tc.server_round,			
+		tc.shuffle,
+		tc.batch_size,
+		tc.initial_epoch,
+		tc.epochs,
+		tc.steps_per_epoch, 
+		tc.validation_split,
+		tc.validation_batch_size
+	FROM
+		otrainingconfig as tc)
+INTO 'training_config.csv' ON CLIENT USING DELIMITERS ',', '\n', '"';
